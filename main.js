e11822db937ab50a4a6d5ba4aecbcfbae7da36a2
JPcookie290 <julia.paier@yahoo.com>
2023-05-26 14:05:20 +0200

    erster Aufbau

--- main.js
+++ main.js
@@ -1,48 +1,62 @@
 "use strict";
 
+let rock = document.getElementById('rock');
+let paper = document.getElementById('paper');
+let scissor = document.getElementById('scissor');
+let punktePc = document.getElementById('punktePc');
+let punktePr = document.getElementById('punktePr');
+let info = document.getElementById('textInfo');
+let reset = document.getElementById('reset');
+
+let computerWin = 0;
+let playerWin = 0;
 /* function for random computer choice */
+
 let computerPlay = () => {
   let computerChoice = ["schere", "stein", "papier"];
   let computerRoll = Math.floor(Math.random() * 3);
   return computerChoice[computerRoll];
-};
+}
+/* function for player choice */
+
+let playerRoll = "";
+rock.onclick = function rock() {
+  playerRoll = "stein";
+  game();
+}
+paper.onclick = function paper() {
+  playerRoll = "papier";
+  game();
+}
+scissor.onclick = function scissor() {
+  playerRoll = "schere";
+  game();
+}
 
 /* function for a single round of the game */
 
-let playRound = (computerRoll) => {
-  let playerRoll = prompt(
-    "Geben Sie Ihre Wahl ein (Schere / Stein / Papier):"
-  ).toLowerCase();
-  while (
-    playerRoll != "schere" &&
-    playerRoll != "stein" &&
-    playerRoll != "papier"
-  ) {
-    playerRoll = prompt(
-      "Bitte geben Sie eine gültige Wahl ein (Schere / Stein / Papier):"
-    ).toLowerCase();
-  }
+let playRound = (playerRoll, computerRoll) => {
   let ergebnis = 0;
   if (playerRoll === "schere" && computerRoll === "stein") {
-    console.log("Du verlierst! - Stein schlägt Schere");
+    info.innerHTML = "Computer wählt Stein" + "<br>" + "<br>" + "Du verlierst! - Stein schlägt Schere"
     ergebnis = 1;
   } else if (playerRoll === "schere" && computerRoll === "papier") {
-    console.log("Du gewinnst! - Schere schlägt Papier");
+    info.innerHTML = "Computer wählt Papier" + "<br>" + "<br>" + "Du gewinnst! - Schere schlägt Papier"
     ergebnis = 2;
   } else if (playerRoll === "stein" && computerRoll === "papier") {
-    console.log("Du verlierst! - Papier schlägt Stein");
+    info.innerHTML = "Computer wählt Papier" + "<br>" + "<br>" + "Du verlierst! - Papier schlägt Stein"
     ergebnis = 1;
   } else if (playerRoll === "stein" && computerRoll === "schere") {
-    console.log("Du gewinnst! - Stein schlägt Schere");
+    info.innerHTML = "Computer wählt Schere" + "<br>" + "<br>" + "Du gewinnst! - Stein schlägt Schere"
     ergebnis = 2;
   } else if (playerRoll === "papier" && computerRoll === "stein") {
-    console.log("Du gewinnst! - Papier schlägt Stein");
+    info.innerHTML = "Computer wählt Stein" + "<br>" + "<br>" + "Du gewinnst! - Papier schlägt Stein"
     ergebnis = 2;
   } else if (playerRoll === "papier" && computerRoll === "schere") {
-    console.log("Du verlierst! - Schere schlägt Papier");
+    info.innerHTML = "Computer wählt Schere" + "<br>" + "<br>" + "Du verlierst! - Schere schlägt Papier"
     ergebnis = 1;
   } else if (playerRoll === computerRoll) {
-    console.log("Unentschieden!");
+    info.innerHTML = "Unentschieden!"
     ergebnis = 0;
   }
   return ergebnis;
@@ -51,30 +65,36 @@ let playRound = (computerRoll) => {
 /* function for the whole game */
 
 let game = () => {
-  let computerWin = 0;
-  let playerWin = 0;
   let winner = "";
-  let winSelect = parseInt(
-    prompt("Geben Sie ein, wie viele Siege nötig sind um zu gewinnen:")
-  );
-  while (computerWin != winSelect && playerWin != winSelect) {
-    /* play until one has five points */
-    let ergebnis = playRound(computerPlay());
+  /* play until one has five points */
+  if (computerWin < 5 && playerWin < 5) {
+    let ergebnis = playRound(playerRoll, computerPlay());
     if (ergebnis === 1) {
       computerWin += 1;
+      punktePc.textContent = computerWin;
     } else if (ergebnis === 2) {
       playerWin += 1;
+      punktePr.textContent = playerWin;
     }
   }
-
+  
   /* winning conditions */
 
-  if (computerWin === winSelect) {
+  if (computerWin === 5) {
     winner = "Der Computer hat gewonnen - " + computerWin + " zu " + playerWin;
-  } else if (playerWin === winSelect) {
+  } else if (playerWin === 5) {
     winner = "Sie haben gewonnen - " + playerWin + " zu " + computerWin;
   }
   return winner;
 };
 
-//console.log(game());
+/* reset */
+
+reset.onclick = function resetAll() {
+  computerWin = 0;
+  punktePc.textContent = computerWin;
+  playerWin = 0;
+  punktePr.textContent = playerWin;
+  info.innerHTML = ""
+  console.log("reset");
+}
